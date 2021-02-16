01727650a66d506b56a8372f25780be169816fa0
emiliarepo <jaqreven@gmail.com>
2021-02-16 21:18:39 +0200

    i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding i hate text encoding

--- discord.py
+++ discord.py
@@ -17,11 +17,15 @@ class Discord:
         self.cleared = True
 
     def setTrack(self, track):
-        print("Changing track to: " + track.track + " by " + track.artist + "(" + track.artist.lower().replace(' ', '_') + ")")
+
+        # thanks stack over flow ,
+        trackName = track.track.encode().decode("unicode-escape").encode("ISO-8859-1").decode("utf8")
+        artistName = track.artist.encode().decode("unicode-escape").encode("ISO-8859-1").decode("utf8")
+        print("Changing track to: " + trackName + " by " + artistName + "(" + artistName.lower().replace(' ', '_') + ")")
         self.cleared = False
         self.rpc.update(
-            state="by " + track.artist,
-            details=track.track + "  ", # discord wants at least 2 characters - add invisible chars!
+            state="by " + artistName,
+            details=trackName + "  ", # discord wants at least 2 characters - add invisible chars!
             start=time.time()-math.ceil(track.position),
             large_image="logo",
             large_text="Apple Music")
